fad6b7dd58ea5bbeb4aee6ecbf711cc1023a1182
Syed Mohammad Easin <syedeasin1000@gmail.com>
2023-11-06 08:54:49 +0600

    Login Screen Creation with Toast Output

---
+++ app/src/main/java/com/Learning/loginApplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.Learning.loginApplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.userName);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("syedeasin") && password.getText().toString().equals("121212"))
+                {
+                    Toast.makeText(MainActivity.this, "Login Successfull",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Login is now Successfull", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
